817267d0a8715346b63ef12456def99eacb1210a
Jesus Rodriguez <Foxandxss@gmail.com>
2014-07-05 14:04:01 +0200

    Now responseError has access to LocalService Shit happen if you don't test my friends

--- frontend/app/js/controllers/loginController.js
+++ frontend/app/js/controllers/loginController.js
@@ -6,10 +6,8 @@ angular.module('app')
       if ($scope.loginForm.$valid) {
         $scope.errors = [];
         Auth.login($scope.user).success(function(result) {
-          console.log("ok");
           $state.go('user.messages');
         }).error(function(err) {
-          console.log("error");
           $scope.errors.push(err);
         });
       }
